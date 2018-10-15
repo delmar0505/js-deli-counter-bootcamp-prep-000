41915170e4d16906cbdb9a7823bb3d91244874fc
oscar leon <first.liberal.thinker@gmail.com>
2018-10-15 22:05:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+/*var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, newName){
+  katzDeliLine.push(['ada'])
+}
+*/
+
+var katzDeliLine = [];
+
+ function takeANumber(katzDeliLine, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+}
